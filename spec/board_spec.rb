9a9d2ef0256e213a817b85149bdb3cfb41ea6446
Wesley <matthew282@msn.com>
2021-12-03 10:49:05 -0700

    updated board_spec

--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -8,11 +8,23 @@ require_relative '../lib/player.rb'
 
 describe Board do
 
+	describe '#check_win' do
+
+		subject(:board_check_win) {Board.new}
+		let(:player) {Player.new("wes", "X")}
 
 
+		context 'when the player has winning moves in moves list' do
 
+			before do
+				player.instance_variable_set(:@moves, [1, 2, 3])
+			end
 
-
+			it 'returns a win message' do
+				expect
+			end
+		end
+	end
 end
 
 
