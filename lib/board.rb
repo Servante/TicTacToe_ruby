0f94883b1afa8b6306b352031c3687d48a10a3d9
Wesley <matthew282@msn.com>
2021-12-02 22:27:18 -0700

    added WINNING_COMBOS

--- lib/board.rb
+++ lib/board.rb
@@ -7,6 +7,8 @@ class Board
 		@cells = [1,2,3,4,5,6,7,8,9]
 	end
 
+	WINNING_COMBOS = [[1,2,3], [4,5,6], [7,8,9], [1,4,7], [1,5,9], [2,5,8], [3,6,9], [3,5,7]].freeze
+
 	def show
 
 		puts <<-HEREDOC
