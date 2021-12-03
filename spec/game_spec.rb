56d6ea3a644490d0886881642697ab79df31772b
Wesley <matthew282@msn.com>
2021-12-02 21:34:12 -0700

    finished test coverage for #player_creation

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -5,12 +5,12 @@ require_relative '../lib/game.rb'
 require_relative '../lib/board.rb'
 require_relative '../lib/player.rb'
 
-
+require 'pry'
 
 describe Game do
 	
 	describe '#player_creation' do
-		subject(:player_prompt) {Game.new}
+		subject(:player_prompt) {described_class.new}
 
 		# context 'when a request for a new player is called' do
 		# 	it 'it uses a module method' do
@@ -20,13 +20,13 @@ describe Game do
 		# 	end
 		# end
 
-		context 'when a request for the first player is invoked' do
-
-			before do
+		before do
 				name = "fred"
 				allow(player_prompt).to receive(:puts)
 				allow(player_prompt).to receive(:gets).and_return(name)
 			end
+
+		context 'when a request for the first player is invoked' do
 
 			it 'assigns the token "X" to first player' do
 				first_player = player_prompt.player_creation(1)
@@ -43,16 +43,27 @@ describe Game do
 
 			let(:first_player) {instance_double(Player)}
 
+			before do 
+				player_prompt.instance_variable_set(:@player1, first_player)
+				player_prompt.instance_variable_set(:@current_player, first_player)
+			end
+
 			it 'assigns the token "O" to second player' do
-				new_game = Game.new(first_player)
 				second_player = player_prompt.player_creation(2)
 				expect(second_player.token).to eq("O")
 			end			
 
-			xit 'does not assign second player as current_player' do
+			it 'does not assign second player as current_player' do
+				second_player = player_prompt.player_creation(2)
+				expect(player_prompt.current_player).not_to be(second_player)
 			end
+		end
 
-			xit 'returns the player object' do				
+		context 'when the script is complete' do
+
+			it 'returns the player object' do
+				returned_object = player_prompt.player_creation(1)
+				expect(returned_object).to be_an_instance_of(Player)
 			end
 		end
 	end
