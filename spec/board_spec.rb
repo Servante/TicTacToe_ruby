e2fff67a02cfe2a4b6a694ce80d7022ebc77eca8
Wesley <matthew282@msn.com>
2021-12-03 10:33:45 -0700

    added board_spec.rb

---
+++ spec/board_spec.rb
@@ -0,0 +1,19 @@
+# frozen_string_literal: true
+
+
+require_relative '../lib/game.rb'
+require_relative '../lib/board.rb'
+require_relative '../lib/player.rb'
+
+
+describe Board do
+
+
+
+
+
+
+end
+
+
+
