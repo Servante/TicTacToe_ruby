ec035770919c639e7919f942f68a8558aa5251b3
Wesley <matthew282@msn.com>
2021-12-02 22:04:20 -0700

    altered #show

--- lib/board.rb
+++ lib/board.rb
@@ -11,11 +11,11 @@ class Board
 
 		puts <<-HEREDOC
 
-		#{cells[0]}  | #{cells[1]} | #{cells[2]}
-		---+---+---
-		#{cells[3]}  | #{cells[4]} | #{cells[5]}
-		---+---+---
-		#{cells[6]}  | #{cells[7]} | #{cells[8]}
+		#{cells[0]} | #{cells[1]} | #{cells[2]}
+		--+---+--
+		#{cells[3]} | #{cells[4]} | #{cells[5]}
+		--+---+--
+		#{cells[6]} | #{cells[7]} | #{cells[8]}
 
 
 		HEREDOC
